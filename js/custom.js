699ee6cc73cd91e6199603960681ce9958b7d31f
GandLWebsite <john@johnmwalsh.ca>
2017-04-24 05:31:15 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 
- /* jQuery Pre loader
-  -----------------------------------------------*/
-$(window).load(function(){
+/* jQuery Pre loader
+ -----------------------------------------------*/
+$(window).load(function () {
     $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
 
 /* Mobile Navigation
-    -----------------------------------------------*/
-$(window).scroll(function() {
+ -----------------------------------------------*/
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -17,110 +17,117 @@ $(window).scroll(function() {
 });
 
 /* HTML document is loaded. DOM is ready. 
--------------------------------------------*/
-$(document).ready(function() {
+ -------------------------------------------*/
+$(document).ready(function () {
 
-  /* Hide mobile menu after clicking on a link
-    -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
+    /* Hide mobile menu after clicking on a link
+     -----------------------------------------------*/
+    $('.navbar-collapse a').click(function () {
         $(".navbar-collapse").collapse('hide');
     });
 
 
- /* Parallax section
-    -----------------------------------------------*/
-  function initParallax() {
-    $('#intro').parallax("100%", 0.1);
-    $('#overview').parallax("100%", 0.3);
-    $('#bridalparty').parallax("100%", 0.1);
-    $('#Accommodations').parallax("100%", 0.1);
-    $('#thingstd').parallax("100%", 0.3);
-    $('#registry').parallax("100%", 0.3);
-    $('#rsvp').parallax("100%", 0.2);
+    /* Parallax section
+     -----------------------------------------------*/
+    function initParallax() {
+        $('#intro').parallax("100%", 0.1);
+        $('#overview').parallax("100%", 0.3);
+        $('#bridalparty').parallax("100%", 0.1);
+        $('#Accommodations').parallax("100%", 0.1);
+        $('#thingstd').parallax("100%", 0.3);
+        $('#registry').parallax("100%", 0.3);
+        $('#rsvp').parallax("100%", 0.2);
 
-  }
-  initParallax();
+    }
+    initParallax();
 
 
-  /* Owl Carousel
-  -----------------------------------------------*/
-  $(document).ready(function() {
-    $("#owl").owlCarousel({
-      autoPlay: 10000,
-      items : 4,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [979,1],
-      itemsTablet: [768,1],
-      itemsTabletSmall: [985,2],
-      itemsMobile : [479,1]
+    /* Owl Carousel
+     -----------------------------------------------*/
+    $(document).ready(function () {
+        $("#owl").owlCarousel({
+            autoPlay: 10000,
+            items: 4,
+            itemsDesktop: [1199, 2],
+            itemsDesktopSmall: [979, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: [985, 2],
+            itemsMobile: [479, 1]
+        });
     });
-  });
-  
-  /* Add guest
-  -----------------------------------------------*/
-jQuery(function(){
+
+    /* Add guest
+     -----------------------------------------------*/
     var counter = 1;
-    jQuery('.addguest').click(function(event){
-        event.preventDefault();
-
-        var newRow = jQuery('<tr><td><input type="text" name="guest' +
-            counter + '"/></td></tr>');
+    $(document).ready(function () {
+        
+        $("#addRow").click(function () {
+            
+            var newRow = jQuery('<tr><td><input autocomplete="off"  name="guest' +
+                    counter + '"\n\
+                    id="guest' +
+                    counter + '"\n\
+                    type="text" /></td></tr>');
+            
+            var oClone = document.getElementById("template").cloneNode(true);
+            oClone.id += (counter + "");
+            oClone.
             counter++;
-        jQuery('.guestTable').append(newRow);
-
+            document.getElementById("placeholder").appendChild(oClone);          
+            $("#guestTable").append(newRow);
+        });
     });
-});
-  /* Back top
-  -----------------------------------------------*/
-    $(window).scroll(function() {
+    /* Back top
+     -----------------------------------------------*/
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-        $('.go-top').fadeIn(200);
+            $('.go-top').fadeIn(200);
         } else {
-          $('.go-top').fadeOut(200);
+            $('.go-top').fadeOut(200);
         }
-        });   
-        // Animate the scroll to top
-      $('.go-top').click(function(event) {
+    });
+    // Animate the scroll to top
+    $('.go-top').click(function (event) {
         event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
-      });
-
-
-  /* wow
-  -------------------------------*/
-  new WOW({ mobile: false }).init();
-  
-
-  
-    /* List of Guests
-  ------------------------*/
-    var availableGuests = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#guests" ).autocomplete({
-      source: availableGuests
+        $('html, body').animate({scrollTop: 0}, 300);
     });
 
-  });
+
+    /* wow
+     -------------------------------*/
+    new WOW({mobile: false}).init();
+
+
+
+    /* List of Guests
+     ------------------------*/
+    var availableGuests = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#guests").autocomplete({
+        source: availableGuests
+    });
+
+});
 
